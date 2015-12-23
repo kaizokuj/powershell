5d0b287c7f3f7c18f9fd2cd9187e8b8e25c3f88e
Kaizokuj <joey.pelgrims@gmail.com>
2015-12-23 14:07:24 +0100

    Added Work-in-progress of Get-HelpFiles

---
+++ Get-HelpFiles/Get-HelpFilesv2.1.3.ps1
@@ -0,0 +1,57 @@
+﻿[CmdletBinding()]
+
+PARAM (
+
+[STRING]$Module = "*",
+[STRING]$Target = "C:\Cmdlets\",
+[STRING]$Command = "*",
+[SWITCH]$NoUpdate,
+[SWITCH]$Sort
+
+)
+
+function GetTime {
+    
+        Return (Get-Date).ToShortTimeString()
+}
+
+If(!($NoUpdate)){
+
+    write-host "[" (GetTime) "] Updating Help Files ..."
+
+    update-help -Module $Module
+
+    write-host -foregroundcolor green "[" (GetTime) "] Help Files Succesfully Updated!"
+
+}
+    
+    if ($Module -ne "*" ) {
+
+        write-host "[" (GetTime) "] Module to be used : $Module"
+
+    }
+
+
+    if (TEST-PATH $Target) {
+
+        Write-Host "[" (getTime) "] The system will now create your helpfiles."
+
+        Get-Command -Module $Module $command | ?{$_.commandtype -ne "Alias"} | ?{$_.ModuleName -ne ""} | %{$Name = $_.Name; Get-Help -full $Name | Out-File "$target\$Name.txt"}
+
+        write-host -foregroundcolor green "[" (GetTime) "] Your help files were created in $target"
+
+    }
+
+else {
+
+   write-host -foregroundcolor Yellow "[" (GetTime) "] No such directory was found, creating now ..."
+   
+   mkdir $Target | out-null
+
+   Write-Host "[" (GetTime) "] The system will now create your helpfiles."
+   
+   (Get-Command -Module $Module $command | ?{$_.commandtype -ne "Alias"}) | ?{$_.ModuleName -ne ""} | %{$Name = $_.Name; Get-Help -full $Name | Out-File "$target\$Name.txt"}
+   
+   write-host -foregroundcolor green "[" (GetTime) "] Your help files were created in $target"
+
+   }

6712b80d70cb93c8955cb53f8004058d2a6aa58a
Kaizokuj <joey.pelgrims@gmail.com>
2016-01-01 00:12:16 +0100

    Uploading a collection of small scripts I've made for myself to get info from AD Servers

---
+++ Misc Scripts/Get-ADUserEmail.ps1
@@ -0,0 +1,11 @@
+﻿[CmdletBinding()]
+
+#This is a script designed to quickly lookup user email addresses
+
+PARAM (
+
+[STRING]$email = "*"
+)
+
+ 
+Get-ADUser -filter {mail -like $email} -Properties EmailAddress| sort-object |  format-table -Property Name, Emailaddress
---
+++ Misc Scripts/Get-ADUserLastLogin.ps1
@@ -0,0 +1,12 @@
+﻿[CmdletBinding()]
+
+#This is a script designed to quickly lookup last user login
+#This script is not working yet
+
+PARAM (
+
+[STRING]$email = "*"
+)
+
+ 
+get-aduser -filter {LastLogon -like "*"}

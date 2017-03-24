141a3853a9139ea64d422c46fda1610ab1467a46
kaizokuj <joey.pelgrims@gmail.com>
2017-03-23 19:40:28 -0700

    Added usage information for Get-Helpfiles.

--- Get-HelpFiles/Get-HelpFilesv2.1.3.ps1
+++ Get-HelpFiles/Get-HelpFilesv2.1.3.ps1
@@ -1,12 +1,38 @@
 ﻿Function get-Helpfiles {
+<#
+.SYNOPSIS
+Creates .txt file version of one or more Get-Help commands.
+.DESCRIPTION
+The cmdlet tries to update help files then for each command in the specified module creates text files with full usage documentation.
+.NOTES
+If no module or command is specified the command will create help files for ALL commands in ALL modules.
+.EXAMPLE
+get-Helpfiles
+Creates help files for every command in every available module.
+.EXAMPLE
+get-helpfiles -Module dnsserver
+Creates help files for all commands in the "dnsserver" module.
+.EXAMPLE
+get-helpfiles -Module dnsserver -command Add-DnsServerClientSubnet
+Creates help file only for "Add-DnsServerClientSubnet" in the "dnsserver" module
+.EXAMPLE
+get-helpfiles -NoUpdate
+Creates all command help files while skipping the updating of help files.
+.LINK
+Get-Command
+Get-help 
 
+#>
 [CmdletBinding()]
 
 PARAM (
-
+#[Parameter(Mandatory=$true,Position=0,ValueFromPipeline=$false,ValueFromPipelineByPropertyName=$false)]
 [STRING]$Module = "*",
+#[Parameter(Mandatory=$true,Position=1,ValueFromPipeline=$false,ValueFromPipelineByPropertyName=$false)]
 [STRING]$Target = "C:\Cmdlets\",
+#[Parameter(Mandatory=$true,Position=2,ValueFromPipeline=$false,ValueFromPipelineByPropertyName=$false)]
 [STRING]$Command = "*",
+#[Parameter(Mandatory=$false,Position=3,ValueFromPipeline=$false,ValueFromPipelineByPropertyName=$false)]
 [SWITCH]$NoUpdate
 
 )

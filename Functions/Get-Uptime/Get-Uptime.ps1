eab55228b0d4fcb03b88d5f2a9fb31910eb92e87
kaizokuj <joey.pelgrims@gmail.com>
2018-03-16 23:16:42 +0100

    Added function to calculate current amount of logged in users

--- Functions/Get-Uptime/Get-Uptime.ps1
+++ Functions/Get-Uptime/Get-Uptime.ps1
@@ -4,13 +4,22 @@ Function Get-Uptime {
     $LastBootUpTime = (gcim Win32_OperatingSystem).LastBootUpTime
     $Uptime = $CurrentDate - $LastBootUpTime
     $ProcessorLoad = Get-CimInstance -ClassName win32_processor | select -ExpandProperty LoadPercentage
-    $Users = query user
+    [System.Collections.ArrayList]$Users = query user
 
     Set-Alias -Name "Get-Uptime" -Value "uptime"
 
+    Function CalculateUsers {
+
+        $Users.RemoveAt(0)
+        $Users.Count
+
+
+    }
+
+
 #Uptime output in linux: $CurrentTime up $days, $hour:$Minutes
 
-$Output = $($CurrentDate.Toshorttimestring()) + " up " + $Uptime.Days + " days, " + $Uptime.Hours + ":" + $Uptime.Minutes + ", Current CPU Usage: " + $ProcessorLoad + "%"
+$Output = $($CurrentDate.Toshorttimestring()) + " up " + $Uptime.Days + " days, " + $Uptime.Hours + ":" + $Uptime.Minutes + " , " + (CalculateUsers) + " users " + ", current CPU usage: " + $ProcessorLoad + "%"
 
     $Output
 

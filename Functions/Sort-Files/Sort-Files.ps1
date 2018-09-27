6dafe3eaef7e89e86e3c6a9ffbea4ab6fdf2af31
Joey Pelgrims <joey.pelgrims@gmail.com>
2018-09-27 09:16:05 +0200

    Finished and moved Sort-Files

---
+++ Functions/Sort-Files/Sort-Files.ps1
@@ -0,0 +1,57 @@
+Function Sort-Files {
+
+    [CmdletBinding(SupportsShouldProcess)]
+
+    param (
+
+        [Parameter(Mandatory = $True)]$Path
+
+    )
+
+    #Define static variables
+    [System.Collections.ArrayList]$Extensions = @('')
+    $Items = Get-ChildItem -file -Path $Path
+
+    foreach ($Item in $Items) {
+
+        if (!($Extensions -contains $Item.Extension)) {
+
+            $Extensions.Add($Item.Extension) | out-null
+            
+            Try {
+
+            mkdir -path $path\ -Name $Item.Extension -ErrorAction SilentlyContinue | Out-Null
+
+            }
+
+            Catch {
+
+                Write-Host -ForegroundColor Yellow "Operation Cancelled: " $Error[0]
+
+            }
+        }
+
+        else {
+
+            
+
+        }
+
+        $destination = $Path + "\" + $Item.Extension + "\" + $Item.Name
+
+        Try {
+
+            Move-Item -Path $Item.FullName -Destination $destination
+
+        }
+
+        Catch {
+
+            Write-Host -ForegroundColor Yellow "Operation Cancelled: " $Error[0]
+
+        }
+        
+
+}
+
+}

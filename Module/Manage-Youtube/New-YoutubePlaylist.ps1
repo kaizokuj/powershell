9631cec5a99f01e9fbf8ba3cbb5406c181db95bc
Joey Pelgrims <joey.pelgrims@gmail.com>
2019-02-26 21:34:47 +0100

    Created functions to manage youtube

---
+++ Module/Manage-Youtube/New-YoutubePlaylist.ps1
@@ -0,0 +1,23 @@
+Function New-YoutubePlaylist {
+
+    param (
+
+        [parameter(Mandatory=$True)]$Accesstoken,
+        [parameter(Mandatory=$True)]$Name,
+        [parameter(Mandatory=$False)]$Description = ""
+
+    )
+
+    <# $body = @{snippet=@{title="$Name"}} | convertto-json #>
+
+    $body = @{snippet=@{
+        title="$Name"
+        description="$Description"
+        }
+    }
+
+
+    Invoke-WebRequest -Uri "https://www.googleapis.com/youtube/v3/playlists?part=snippet" -Method Post -Body ($body | ConvertTo-Json) -Headers @{Authorization = "Bearer $accesstoken"} -ErrorVariable RespErr -ContentType "application/json"
+
+
+}
